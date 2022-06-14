adac77d843b8603987f3b12b27b211c85cb1842d
usmarmanalu <usmarmnl99@gmail.com>
2022-06-14 23:41:42 +0700

    add comentar pada script

--- script/script.js
+++ script/script.js
@@ -1,19 +1,28 @@
+// MEMANIPULASI DOM (Document Object Model)
+
+// Mengembalikan tiap-tiap elemen dalam dokument yang cocok dengan selector CSS
 const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
+const menuList = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 
+// class menu-btn ketika di klik
 menuBtn.onclick = () => {
-	menu.classList.add("active");
-	menuBtn.classList.add("hide");
-	body.classList.add("disabledScroll");
+	menuList.classList.add("active"); //mengembalikan fungsi class menu-list dan menambahkan class active
+	menuBtn.classList.add("hide"); //mengembalikan fungsi class menu-btn dan menambahkan class hide
+	body.classList.add("disabledScroll"); //mengembalikan fungsi element body html dan menambahkan class disabledScroll
 };
+
+// class cancel-btn ketika di klik
 cancelBtn.onclick = () => {
-	menu.classList.remove("active");
-	menuBtn.classList.remove("hide");
-	body.classList.remove("disabledScroll");
+	menuList.classList.remove("active"); //mengembalikan fungsi class menu-list dan menyembunyikan class active
+	menuBtn.classList.remove("hide"); //mengembalikan fungsi class menu-btn dan menyembunyikan class hide
+	body.classList.remove("disabledScroll"); //mengembalikan fungsi element body html dan menyembunyikan class active
 };
+
+// Indikator Scrool
+// selector sticky akan tampil jika melakukan scroll pada halaman web sebesar 20% secara vertikal
 window.onscroll = () => {
 	this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 };
